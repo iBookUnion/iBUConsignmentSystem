3e4c0a72746ed5ed6ce3da03c6f68c88b9fa719e
Lisa <li.mw.lisa@gmail.com>
2014-12-11 05:51:40 -0800

    full consignment form with sparse validation

--- js/consignApp.js
+++ js/consignApp.js
@@ -1,28 +1,91 @@
 (function(){
 	var app = angular.module('consignmentApp', ['ui.bootstrap']);
+
 	app.controller('ConsignmentCtrl', function($scope, $log) {
-		$scope.books = bookList;
-		$scope.consignedBook = {
-			'isbn': '',
-			'course': '',
-			'price': '',
-			'title': '',
-			'author': ''
+		signContract = false;
+
+		$scope.contact = {
+			'fname': '',
+			'lname': '',
+			'sno': '',
+			'email': '',
+			'phno': '',
+			'faculty': '',
+			'discovery': ''
 		};
+
+		$scope.fname = fname;
+		console.log($scope.contact.fname);
+
+		$scope.createJson = function() {
+			consignor = {'student_id': $scope.contact.sno,
+						'first_name': $scope.contact.fname,
+						'last_name': $scope.contact.lname,
+						'email': $scope.contact.email,
+						'phone_number': $scope.contact.phno};
+			books = [];
+			consignments = [];
+
+			for (i = 0; i < bookList.length; i++) {
+				book = {
+					'isbn': bookList[i].isbn,
+					'title': bookList[i].title,
+					'author': bookList[i].author,
+					'edition': 1,
+					'courses': bookList[i].courses			
+				};
+
+				consignment = {
+					'isbn': bookList[i].isbn,
+					'student_id': $scope.contact.sno,
+					'price': bookList[i].price,
+					'current_state': '1',
+					'date': 'Now'
+				}
+
+				books.push(book);
+				consignments.push(consignment);
+			}
+
+			jsonObj = {'consignor': consignor,
+						'books': books,
+						'consignments': consignments};
+
+			console.log(JSON.stringify(jsonObj));
+
+			var confirmation = window.open();
+			confirmation.document.write(JSON.stringify(jsonObj));
+		}
+
+	});
+
+	// app.controller('ContactCtrl', function($scope, $log) {
+	// 	$scope.fname = fname;
+	// 	$scope.lname = lname;
+	// 	$scope.sno = sno;
+	// 	$scope.email = email;
+	// 	$scope.phno = phno;
+	// 	$scope.faculty = faculty;
+	// 	$scope.discover = discovery;
+
+	// });
+
+	app.controller('BookFormCtrl', function($scope, $log) {
+		$scope.books = bookList;
 
 	});
 
 	app.controller('ConsignFormCtrl', function($log) {
 		this.consignedBook = {
 			isbn: '',
-			course: '',
+			courses: [],
 			price: '',
 			title: '',
 			author: ''
 		};
 
 		this.addBook = function() {
-			$log.info('Consigning book ' + this.consignedBook.isbn + " for course" + this.consignedBook.course);
+			$log.info('Consigning book ' + this.consignedBook.isbn + " for course" + this.consignedBook.courses[0]);
 			bookList.push(this.consignedBook);
 			this.consignedBook = {};
 		}
@@ -57,5 +120,13 @@
 	});
 
 	var bookList = [];
+	var contactInfo = [];
+	var fname = '';
+	var lname = '';
+	var sno = '';
+	var email = '';
+	var phno = '';
+	var faculty = '';
+	var discovery = '';
 
 })()
