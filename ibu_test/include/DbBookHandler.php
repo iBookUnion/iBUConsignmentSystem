caa561e0ef46a2336a69dee45f4a3e5c1472e112
rcacho <rcacho@hotmail.ca>
2015-05-07 08:06:04 +0000

    abstracted dbhandler

---
+++ ibu_test/include/DbBookHandler.php
@@ -0,0 +1,110 @@
+<?php
+
+class DbBookHandler extends Dbhandler {
+
+	protected $conn;
+	protected $table = "books";
+    protected $key = "isbn";
+
+	function __construct() {
+        require_once dirname(__FILE__) . './DbConnect.php';
+        // opening db connection
+        $db = new DbConnect();
+        $this->conn = $db->connect();
+    }
+
+    protected function set_conditions($query_params) {
+    	$conditions = array();
+
+		$conditions[] = $this->set_isbn($query_params["isbn"]);
+		$conditions[] = $this->set_author($query_params["author"]);
+		$conditions[] = $this->set_title($query_params["title"]);
+		$conditions[] = $this->set_edition($query_params["edition"]);
+
+			return $conditions; 
+    }
+
+    protected function package_result($stmt) {
+		$rows = $stmt->num_rows;
+		$stmt->bind_result($isbn, $title, $author, $edition, $courses);
+		
+		$books = array();
+		while($row = $stmt->fetch())
+		{
+			$book["isbn"] = $isbn;
+			$book["title"] = $title;
+			$book["author"] = $author;
+			$book["edition"] = $edition;
+			$book["courses"] = $courses;
+			$books[] = $book;
+		}
+		
+		$stmt->close();
+			return $books;	
+    }
+
+    protected function obtain_key($params) {
+        $k = $params[$this->key];
+            return $k;
+    }
+
+    protected function get_table() {
+    		return $this->table;
+    }
+
+    protected function get_columns() {
+        $columns = "(isbn, title, author, edition, courses)";
+            return $columns;
+    }
+
+    protected function get_search_array($key) {
+        $query_params = array("isbn" => $key,
+                              "title" => null,
+                              "author" => null,
+                              "edition" => null,
+                              "courses" => null);
+            return $query_params;
+    }
+
+    protected function prepare_strings($params) {
+        $params["title"] = $this->stringify($params["title"]);  
+        $params["author"] =  $this->stringify($params["author"]);
+            return $params;
+    }
+
+    private function set_isbn($query_param) {
+    	if ($query_param != null) {
+    		$cond = "isbn = " . $query_param;
+                return $cond;
+    	}
+    		
+    }
+
+	private function set_author($query_param) {
+    	if ($query_param != null) {
+    		$cond = "author = " . $this->stringify($query_param);
+                return $cond;
+    	}
+    		
+	}
+
+	private function set_title($query_param) {
+    	if ($query_param != null) {
+    		$cond = "title = " . $this->stringify($query_param);
+                return $cond;
+    	}
+    		
+	}
+
+	private function set_edition($query_param) {
+    	if ($query_param != null) {
+    		$cond = "edition = " . $this->stringify($query_param);
+                return $cond;
+    	}
+    				
+	}
+
+}
+
+
+?>
---
+++ ibu_test/include/DbUserHandler.php
@@ -0,0 +1,118 @@
+<?php
+
+class DbUserHandler extends DbHandler {
+
+	protected $conn;
+	protected $table = "users";
+	protected $key = "student_id";
+
+	function __construct() {
+        require_once dirname(__FILE__) . './DbConnect.php';
+        // opening db connection
+        $db = new DbConnect();
+        $this->conn = $db->connect();
+    }
+
+
+    protected function set_conditions($query_params) {
+		$conditions = array();
+
+		$conditions[] = $this->set_student_id($query_params["student_id"]);
+		$conditions[] = $this->set_first_name($query_params["first_name"]);
+		$conditions[] = $this->set_last_name($query_params["last_name"]);
+		$conditions[] = $this->set_email($query_params["email"]);
+		$conditions[] = $this->set_phone_number($query_params["phone_number"]);
+
+			return $conditions;
+    }
+
+
+    protected function package_result($stmt) {
+
+    	$rows = $stmt->num_rows;
+		$stmt->bind_result($student_id, $first_name, $last_name, $email, $phone_number);
+		
+		$users = array();
+
+		while($row = $stmt->fetch())
+		{
+			$user["student_id"] = $student_id;
+			$user["first_name"] = $first_name;
+			$user["last_name"] = $last_name;
+			$user["email"] = $email;
+			$user["phone_number"] = $phone_number;
+			$users[] = $user;	
+		}
+		
+		$stmt->close();		
+
+			return $users;		
+		
+    }
+
+    protected function obtain_key($params) {
+    	$k = $params[$this->key];
+    		return $k;
+    }
+
+    protected function get_table() {
+    		return $this->table;
+    }
+
+    protected function get_columns() {
+    	$columns = " (student_id, first_name, last_name, email, phone_number) ";
+    		return $columns;
+    }
+
+    protected function get_search_array($key) {
+        $query_params = array("student_id" => $key,
+                     		  "first_name" => null,
+                      		  "last_name" => null,
+                              "email" => null,
+                      		  "phone_number" => null);
+        	return $query_params;
+    }
+
+    protected function prepare_strings($params) {
+    	$params["first_name"] = $this->stringify($params["first_name"]);  
+    	$params["last_name"] =  $this->stringify($params["last_name"]);
+    	$params["email"] = $this->stringify($params["email"]);
+    		return $params;
+    }
+    
+    private function set_student_id($query_param) {
+    	if ($query_param != null) {
+			$cond = "student_id = " . $query_param;
+				return $cond;
+		}
+    }
+
+    private function set_first_name($query_param) {
+    	if ($query_param != null) {
+			$cond = "first_name = " . $this->stringify($query_param);
+				return $cond;
+		}
+
+    }
+    private function set_last_name($query_param) {
+    	if ($query_param != null) {
+			$cond = "last_name = " . $this->stringify($query_param);
+				return $cond;  
+		}  	
+    }
+    private function set_email($query_param) {
+    	if ($query_param != null) {
+			$cond = "email = " . $this->stringify($query_param);
+				return $cond;  
+		}    
+    }
+    private function set_phone_number($query_param) {
+     	if ($query_param != null) {
+			$cond = "phone_number = " . $query_param;
+				return $cond;  
+		}
+    }
+   
+}
+
+?>
