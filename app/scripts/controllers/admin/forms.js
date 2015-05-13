43c4b37dec313e7e15a930dea2bc55afe2914752
Tim C <timch326@gmail.com>
2015-05-13 16:57:01 -0700

    Minor changes - removed dropdown, using label instead for consignment form status.

--- app/scripts/controllers/admin/forms.js
+++ app/scripts/controllers/admin/forms.js
@@ -8,25 +8,28 @@
  * Controller of the consignmentApp
  */
 angular.module('consignmentApp')
-  .controller('FormsCtrl', ['$scope', '$routeParams', '$location', 'Consignors', function ($scope, $routeParams, $location, Consignors) {
+    .controller('FormsCtrl', ['$scope', '$routeParams', '$location', 'Consignors', 'Inventory',
+        function ($scope, $routeParams, $location, Consignors, Inventory) {
 
-    if ($routeParams.isbn) {
-      $scope.isbn = $routeParams.isbn;
-    }
+        if ($routeParams.isbn) {
+            $scope.isbn = $routeParams.isbn;
+            Inventory.get()
+        }
 
-    Consignors.get(function(payload) {
-      $scope.consignors = payload.users.map(function (apiConsignor) {
-        var consignor = {};
-        consignor.studentId = apiConsignor.student_id; // jshint ignore:line
-        consignor.firstName = apiConsignor.first_name; // jshint ignore:line
-        consignor.lastName = apiConsignor.last_name; // jshint ignore:line
-        return consignor;
-      });
-    });
+        Consignors.get(function (payload) {
+            $scope.consignors = payload.users.map(function (apiConsignor) {
+                var consignor = {};
+                consignor.studentId = apiConsignor.student_id; // jshint ignore:line
+                consignor.firstName = apiConsignor.first_name; // jshint ignore:line
+                consignor.lastName = apiConsignor.last_name; // jshint ignore:line
+                return consignor;
+            });
+        });
 
-    $scope.viewConsignor = function() {
-      $location.url($location.path);  // Clear query parameters
-      $location.path('/admin/consignorInfo/' + this.consignor.studentId);
-    };
 
-  }]);
+        $scope.viewConsignor = function () {
+            $location.url($location.path);  // Clear query parameters
+            $location.path('/admin/consignorInfo/' + this.consignor.studentId);
+        };
+
+    }]);
