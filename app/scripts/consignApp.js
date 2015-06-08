63414059715d496c6bd10a9d6211bf7277d75577
Tim C <timch326@gmail.com>
2015-06-07 17:16:30 -0700

    Added status dropdown for books in admin site.

--- app/scripts/consignApp.js
+++ app/scripts/consignApp.js
@@ -1,46 +1,52 @@
 'use strict';
 
 (function () {
-    var app = angular.module('consignmentApp', ['ui.bootstrap', 'ngRoute', 'ngResource']);
+  var app = angular.module('consignmentApp', ['ui.bootstrap', 'ngRoute', 'ngResource']);
 
-    app.config(function ($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'views/consignmentForm.html'
-            })
-            .when('/admin', {
-                templateUrl: 'views/inventory.html',
-                controller: 'InventoryCtrl'
-            })
-            .when('/admin/forms', {
-                templateUrl: 'views/forms.html',
-                controller: 'FormsCtrl'
-            })
-            .when('/admin/consignorInfo/:consignorId', {
-                templateUrl: 'views/consignorInfo.html',
-                controller: 'ConsignorInfoCtrl'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-    });
+  app.config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/consignmentForm.html'
+      })
+      .when('/admin', {
+        templateUrl: 'views/inventory.html',
+        controller: 'InventoryCtrl'
+      })
+      .when('/admin/forms', {
+        templateUrl: 'views/forms.html',
+        controller: 'FormsCtrl'
+      })
+      .when('/admin/consignorInfo/:consignorId', {
+        templateUrl: 'views/consignorInfo.html',
+        controller: 'ConsignorInfoCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
 
-    app.directive('contactForm', function () {
-        return {
-            restrict: 'E',
-            templateUrl: 'views/consignmentForm/contactForm.html'
-        };
+  app.directive('contactForm', function () {
+    return {
+      restrict: 'E',
+      templateUrl: 'views/consignmentForm/contactForm.html'
+    };
+  })
+    .directive('bookList', function () {
+      return {
+        restrict: 'E',
+        templateUrl: 'views/consignmentForm/bookList.html'
+      };
     })
-        .directive('bookList', function () {
-            return {
-                restrict: 'E',
-                templateUrl: 'views/consignmentForm/bookList.html'
-            };
-        })
-        .directive('agreement', function () {
-            return {
-                restrict: 'E',
-                templateUrl: 'views/consignmentForm/agreement.html'
-            };
-        });
+    .directive('agreement', function () {
+      return {
+        restrict: 'E',
+        templateUrl: 'views/consignmentForm/agreement.html'
+      };
+    })
+    .directive('adminBookList', function () {
+      return {
+        restrict: 'E',
+        templateUrl: 'views/admin/adminBookList.html'
+      };
+    });
 })();
