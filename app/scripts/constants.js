11e30a101a3c97c3fbe689687064e0c035d910c3
Tim C <timch326@gmail.com>
2015-06-18 14:03:14 -0700

    Correct consignments are now retrieved when searching for consignments with a certain book. Tests WIP.

--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -1,13 +1,14 @@
 'use strict';
 
 angular.module('consignmentApp')
-    .constant('API_URI', (function () {
-        var baseURL = 'http://timadvance.me/ibu_test/v1';
-        return {
-            'baseURL': baseURL,
-            'inventory': baseURL + '/inventory',
-            'consignors': baseURL + '/users',
-            'consignor': baseURL + '/user/:consignorId',
-            'consignment': baseURL + '/consignments'
-        };
-    })());
+  .constant('API_URI', (function () {
+    var baseURL = 'http://timadvance.me/ibu_test/v1';
+    return {
+      'baseURL': baseURL,
+      'inventory': baseURL + '/inventory',
+      'books': baseURL + '/books/:isbn',
+      'consignors': baseURL + '/users',
+      'consignor': baseURL + '/user/:consignorId',
+      'consignment': baseURL + '/consignments'
+    };
+  })());
