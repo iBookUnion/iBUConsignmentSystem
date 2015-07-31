6219a4de14e9a81ebcfe2086586a9b99503183a1
Tim C <timch326@gmail.com>
2015-07-31 14:34:04 -0700

    Added basic google sign in button.

--- app/scripts/controllers/admin/AdminLoginCtrl.js
+++ app/scripts/controllers/admin/AdminLoginCtrl.js
@@ -1,6 +1,8 @@
 'use strict';
 
 angular.module('consignmentApp')
-  .controller('AdminLoginCtrl', function () {
-
-  });
+  .controller('AdminLoginCtrl', ['$scope', function ($scope) {
+    $scope.$on('event:google-plus-signin-success', function (event,authResult) {
+      console.log('Sign in successful, now we need to work on sending this to the server!');
+    });
+  }]);
