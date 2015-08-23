d414bb7fde50e27967669f6acad8c85fb1467c76
rcacho <rcacho@hotmail.ca>
2015-08-23 03:23:29 +0000

    add disabling to consigmetn submission button

--- app/scripts/controllers/consignmentForm/consignment.js
+++ app/scripts/controllers/consignmentForm/consignment.js
@@ -8,9 +8,11 @@ angular.module('consignmentApp')
       $scope.faculties = OPTIONS.faculties;
 
       $scope.submitForm = function (form) {
+        $scope.flag = true;
         console.log(form);
         ConsignmentService.submitForm(form)
           .then(function (response) {
+            $scope.flag = false;
             console.log(response);
             // set contract to be accessible through ContractService
             console.log(response);
@@ -18,10 +20,11 @@ angular.module('consignmentApp')
             $location.path('/contract');
           },
           function (error) {
+            $scope.flag = false;
             // if form submission fails, then...(TODO)
             console.log(error);
             $location.path('/contract');
-          });
+          })
       };
 
       $scope.$on('$routeChangeSuccess', function () {
